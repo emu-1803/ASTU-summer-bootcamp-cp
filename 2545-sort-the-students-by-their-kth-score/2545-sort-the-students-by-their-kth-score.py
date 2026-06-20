378a38c85897387166acbe50ff2d8e97f360a6e7
emu-1803 <nanabunny6107@gmail.com>
2026-06-20 22:40:16 +0300

    Delete 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        score.sort(key=lambda student: student[k], reverse=True)
-        return score
