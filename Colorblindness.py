20df84496ab10cd893548ee80eb3c22da169e067
emu-1803 <nanabunny6107@gmail.com>
2026-07-05 12:19:35 +0300

    Implement colorblindness check between two strings

---
+++ Colorblindness.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n=int(input())
+    s1=input()
+    s2=input()
+
+    s1=s1.replace('B','G')
+    s2=s2.replace('B','G')
+    
+    if s1 == s2:
+        print("YES")
+    else:
+        print("NO")
