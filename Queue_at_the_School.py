a8fe9bda41b54c99e01759f1d70daa01164acec1
emu-1803 <nanabunny6107@gmail.com>
2026-07-13 20:46:05 +0300

    Create Queue_at_the_School.py

---
+++ Queue_at_the_School.py
@@ -0,0 +1,12 @@
+n,t=map(int,input().split())
+s=list(input())
+i=0
+for i in range(t):
+    i=0
+    while i<n-1:
+        if s[i]=="B" and s[i+1]=="G":
+            s[i] , s[i+1] = s[i+1] , s[i]
+            i+=2
+        else:
+            i+=1
+print("".join(s))
