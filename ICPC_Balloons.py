86868694faadc46f4a02ac98bc9a8e3d0d7b0b99
emu-1803 <nanabunny6107@gmail.com>
2026-07-15 21:00:44 +0300

    Implement balloon counting logic in ICPC_Balloons.py

---
+++ ICPC_Balloons.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n=int(input())
+    s=input().upper()
+    ans=0
+    visited=set()
+    for i in range(n):
+        if s[i] in visited:
+            ans+=1
+        else:
+            ans+=2
+            visited.add(s[i])
+    print(ans)
