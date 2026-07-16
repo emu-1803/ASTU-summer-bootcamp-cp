6db4892c49ff57809620ff27766de966f8db31a5
emu-1803 <nanabunny6107@gmail.com>
2026-07-16 13:15:51 +0300

    Implement logic to remove prefix from list

---
+++ Remove_Prefix.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    n=int(input())
+    a=list(map(int,input().split()))
+    visited=set()
+    ans=0
+
+    for i in range(n-1,-1,-1):
+        if a[i] in visited:
+            ans=i+1
+            break
+        visited.add(a[i])
+
+    print(ans)
